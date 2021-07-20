ce547681d7a942f897cab38d98b5a816dea88c9d
Matthew Helkey <mshelkey@ucdavis.edu>
2021-07-19 18:26:31 -0700

    fix overwriting issue

--- generate-image-html.sh
+++ generate-image-html.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 for filename  in ./images/helkeyphotos/*.jpg; do
-	echo \<img src="${filename}" width="500" height="333"\> > all.html
+	echo "" > all.html #clears the file
+	echo \<img src="${filename}" width="500" height="333"\> >> all.html
 done
