6d0011cec188e00ddbd324899284550743cdb437
Matthew Helkey <mshelkey@ucdavis.edu>
2021-07-21 17:39:49 -0700

    change photo dimensions to native size

---
+++ generate_html_from_images.rb
@@ -0,0 +1,23 @@
+require 'exifr/jpeg'
+
+
+#provide this with path to images.
+#example: '/path/to/dir/*.jpg' will convert all the images
+def generate(pathname,output_file)
+
+  File.open(output_file, 'w') {|file| file.truncate(0) }
+
+  Dir.glob(pathname) do |filename|
+    # Do work on files & directories ending in .jpg
+    
+    html = "<img src=\"#{filename}\"\
+             width=\"#{EXIFR::JPEG.new(filename).width}\"\
+             height=\"#{EXIFR::JPEG.new(filename).height}\"> "
+
+    File.open(output_file, 'a') { |file| file.write(html) }
+
+  end
+end
+
+  
+generate('./images/helkeyphotos/*.jpg', 'all.html')
